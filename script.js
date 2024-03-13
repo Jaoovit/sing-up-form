12d8170dc597d7f55689deb1373f76e118d2cbd2
João Oliveira <jaoovit@gmail.com>
2024-03-13 11:00:51 +0000

    input the matchPassword funtion in the submit buttom

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 function matchPassword() {
     let a = document.querySelector('#password').value
     let b = document.querySelector('#confPassword').value
-    if(a != b)
-    {
+    if(a != b) {
+        a.value = ''
+        b.value = ''
         document.querySelector('.erro').innerHTML = '* Password do not match'
         document.querySelector('.password').style.border = `1px solid red`
         document.querySelector('.confpassword').style.border = `1px solid red`
+    } else {
+        document.querySelector('.erro').innerHTML = ''
+        document.querySelector('.password').style.border = `1px solid gray`
+        document.querySelector('.confpassword').style.border = `1px solid gray`
     }
 }
 
-document.querySelector('.teste').addEventListener('click', function Onclick() {
+document.querySelector('input[type="submit"]').addEventListener('click', function Onclick() {
     matchPassword()
 })
 
